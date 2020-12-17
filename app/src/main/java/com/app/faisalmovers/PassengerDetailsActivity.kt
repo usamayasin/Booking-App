857c6ab740ff707c028c0e0ba65f33ad8f959eb2
usama.yasin <uyasin@pie-int.com>
2020-12-18 01:10:18 +0500

    UI Responsive bug fixing

--- app/src/main/java/com/app/faisalmovers/PassengerDetailsActivity.kt
+++ app/src/main/java/com/app/faisalmovers/PassengerDetailsActivity.kt
@@ -1,11 +1,21 @@
 package com.app.faisalmovers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class PassengerDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_passenger_details)
+
+        val passengerDetailsGoButton: ImageView = findViewById(R.id.iv_passenger_details_go_btn)
+        passengerDetailsGoButton.setOnClickListener {
+            val intent = Intent(this@PassengerDetailsActivity, InvoiceActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
