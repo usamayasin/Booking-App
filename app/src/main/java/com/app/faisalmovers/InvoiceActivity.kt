34db77066b67428e94efd51be1f3aa7f1513a61e
AbuBakarAcc <abubakar.kaleem@acclivousbyte.com>
2020-12-18 00:13:04 +0500

    Invoice Screen UI responsive bug fixing

---
+++ app/src/main/java/com/app/faisalmovers/InvoiceActivity.kt
@@ -0,0 +1,11 @@
+package com.app.faisalmovers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InvoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invoice)
+    }
+}
