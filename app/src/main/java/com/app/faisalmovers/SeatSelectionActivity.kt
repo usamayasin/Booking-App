e7d1fd83c31fa96bc02e4b199ab5df25f0eb8cfd
usama.yasin <uyasin@pie-int.com>
2020-12-20 01:31:33 +0500

    Show gender selection dialog on seat click Done but code is commented

--- app/src/main/java/com/app/faisalmovers/SeatSelectionActivity.kt
+++ app/src/main/java/com/app/faisalmovers/SeatSelectionActivity.kt
@@ -1,17 +1,25 @@
 package com.app.faisalmovers
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 
 class SeatSelectionActivity : AppCompatActivity() {
 
-    private val seatsList = ArrayList<Seat>()
+    private var genderSelectionDialog: Dialog? = null
     private lateinit var seatsAdapter: SeatsAdapter
+    private var ivGenderMale: ImageView? = null
+    private var ivGenderFemale: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +36,7 @@ class SeatSelectionActivity : AppCompatActivity() {
         var selectedSeat: CardView? = null;
         selectedSeat = findViewById(view.id);
         controlSeatSelection(selectedSeat)
+        //showGenderSelectionDialog(this@SeatSelectionActivity, selectedSeat)
     }
 
     fun controlSeatSelection(cardView: CardView) {
@@ -36,5 +45,36 @@ class SeatSelectionActivity : AppCompatActivity() {
         } else {
             cardView.setCardBackgroundColor(Color.WHITE)
         }
+    }
+
+    private fun showGenderSelectionDialog(context: Context, selectedSeat: CardView) {
+
+        genderSelectionDialog = Dialog(context)
+        genderSelectionDialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        genderSelectionDialog?.setContentView(R.layout.gender_selection_dialog)
+        genderSelectionDialog?.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        genderSelectionDialog?.show()
+
+        ivGenderMale = genderSelectionDialog?.findViewById(R.id.iv_gender_male)
+        ivGenderMale?.setOnClickListener {
+            controlSeatSelection(selectedSeat, true)
+        }
+
+        ivGenderFemale = genderSelectionDialog?.findViewById(R.id.iv_gender_female)
+        ivGenderFemale?.setOnClickListener {
+            controlSeatSelection(selectedSeat, false)
+        }
+    }
+    fun controlSeatSelection(cardView: CardView, gender: Boolean) {
+        if (gender) {
+            cardView.setCardBackgroundColor(resources.getColor(R.color.male_text_color))
+        } else {
+            cardView.setCardBackgroundColor(resources.getColor(R.color.female_text_color))
+        }
+
+        val firstChild: LinearLayout = cardView.getChildAt(0) as LinearLayout
+        val tvChild: TextView = firstChild.getChildAt(0) as TextView
+        tvChild.setTextColor(resources.getColor(R.color.white))
+        genderSelectionDialog?.dismiss()
     }
 }
